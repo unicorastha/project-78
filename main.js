3e3ab3e53685a8c962ce504eca4ede9843aaa545
unicorastha <85661393+unicorastha@users.noreply.github.com>
2021-06-18 16:40:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var images=
+["https://i.pinimg.com/originals/38/79/ae/3879ae8a790a7650e5b831adad343ef4.jpg "," https://i.pinimg.com/originals/38/79/ae/3879ae8a790a7650e5b831adad343ef4.jpg ,"
+"https://thumbs.dreamstime.com/z/happy-cute-kid-girl-hugging-mom-love-164772553.jpg" ,"https://thumbs.dreamstime.com/z/cute-little-girl-stuffed-toys-vector-illustration-white-66885900.jpg "
+]
+ var names[" family book" "ravindra" "smita" "astha" "aarya"
+
+ ]
